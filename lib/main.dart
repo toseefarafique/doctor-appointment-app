815d51514fdb5465970371da130453e40faa65a5
Toseefa Rafique <toseefarafiqu@gmail.com>
2026-08-31 16:20:35 +0500

    Connect Flutter app with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 import 'doctor/doctor_main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -23,3 +33,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
